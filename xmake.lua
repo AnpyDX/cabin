b3bc69e76efb66a68c672ad27767ab789f6cdd2d
AnpyDX <anpyd@outlook.com>
2025-07-16 21:40:58 +0800

    update: add packages stb and imgui

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,8 @@ set_languages("c++20")
 set_allowedplats("windows", "linux")
 set_rules("mode.debug", "mode.release")
 
-add_requires("glad", "glfw", "glm")
-add_packages("glad", "glfw", "glm")
+add_requires("glad", "glfw", "glm", "stb")
+add_requires("imgui", {configs = { glfw = true, opengl3 = true }})
+add_packages("glad", "glfw", "glm", "stb", "imgui")
 
 includes("src", "sandbox")
