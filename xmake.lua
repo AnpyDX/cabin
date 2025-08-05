0e0d03c14a133933b8506fea247d311580cd7cc8
AnpyDX <anpyd@outlook.com>
2025-08-05 14:52:18 +0800

    feat(core): use `tinygltf` as `Model`'s model-loader (instead `assimp`)

--- xmake.lua
+++ xmake.lua
@@ -5,9 +5,8 @@ set_languages("c++20")
 set_allowedplats("windows", "linux")
 set_rules("mode.debug", "mode.release")
 
-add_requires("glad", "glfw", "glm", "stb")
+add_requires("glad", "glfw", "glm", "stb", "tinygltf")
 add_requires("imgui", {configs = { glfw = true, opengl3 = true }})
-add_requires("assimp", {configs = { no_export = true }})
-add_packages("glad", "glfw", "glm", "stb", "imgui", "assimp")
+add_packages("glad", "glfw", "glm", "stb", "imgui", "tinygltf")
 
 includes("src", "sandbox")
