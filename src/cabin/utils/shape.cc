6c18c557f164e8a86709bf300ac85cda900d01bf
AnpyDX <anpyd@outlook.com>
2025-10-17 16:07:25 +0800

    update(utils): add `Shape::Plane` and use drawElements as default mode.

--- src/cabin/utils/shape.cc
+++ src/cabin/utils/shape.cc
@@ -2,6 +2,7 @@
 #include "cabin/core/vertexbuffer.h"
 
 #include <cmath>
+#include <utility>
 #include <glad/glad.h>
 #include <glm/geometric.hpp>
 #include <glm/ext/scalar_constants.hpp>
@@ -52,15 +53,12 @@ namespace {
         -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f 
     };
 
-    float SHAPE_PLANE_VERTICES[] = {
-        // Position       Normal          TexCoord
-         1.0,  1.0, 0.0,  0.0, 0.0, 1.0,  1.0, 1.0,
-        -1.0,  1.0, 0.0,  0.0, 0.0, 1.0,  0.0, 1.0,
-        -1.0, -1.0, 0.0,  0.0, 0.0, 1.0,  0.0, 0.0,
-
-         1.0,  1.0, 0.0,  0.0, 0.0, 1.0,  1.0, 1.0,
-        -1.0, -1.0, 0.0,  0.0, 0.0, 1.0,  0.0, 0.0,
-         1.0, -1.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0
+    float SHAPE_SQUARE_VERTICES[] = {
+        /* Position          Normal             TexCoord */
+         1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+        -1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f
     };
 }
 
@@ -73,19 +71,90 @@ namespace cabin::utils {
                                 .addAttribute<float>(1, 3)
                                 .addAttribute<float>(2, 2)
                                 .build();
-        m_count = 36;
+        m_indices.resize(36);
+        for (int i = 0; i < 36; i++) m_indices[i] = i;
 
         return *this;
     }
 
-    Shape::Builder& Shape::Builder::asPlane() {
+    Shape::Builder& Shape::Builder::asSquare() {
         m_vertices = core::VertexBuffer::Builder()
-                                .setBuffer(SHAPE_PLANE_VERTICES, sizeof(SHAPE_PLANE_VERTICES), GL_STATIC_DRAW)
+                                .setBuffer(SHAPE_SQUARE_VERTICES, sizeof(SHAPE_SQUARE_VERTICES), GL_STATIC_DRAW)
                                 .addAttribute<float>(0, 3)
                                 .addAttribute<float>(1, 3)
                                 .addAttribute<float>(2, 2)
                                 .build();
-        m_count = 6;
+
+        m_indices = { 0, 1, 2, 0, 2, 3 };
+        
+        return *this;
+    }
+
+    Shape::Builder& Shape::Builder::asPlane(glm::vec2 size, glm::vec<2, int> division) {
+
+        /* Vertices */
+        std::vector<glm::vec3> positions((division.x + 1) * (division.y + 1));
+        std::vector<glm::vec2> texCoords((division.x + 1) * (division.y + 1));
+
+        for (int i = 0; i <= division.y; i++) {
+            for (int j = 0; j <= division.x; j++) {
+                float k_x = static_cast<float>(j) / division.x;
+                float k_y = static_cast<float>(i) / division.y;
+
+                glm::vec3 pos {};
+                pos.x = size.x * (k_x - 0.5f);
+                pos.y = size.y * (0.5f - k_y);
+                pos.z = 0.0f;
+
+                glm::vec2 texCoord { k_x, 1.0 - k_y };
+
+                int index = i * (division.x + 1) + j;
+                positions[index] = pos;
+                texCoords[index] = texCoord;
+            }
+        }
+
+        struct alignas(4) Vertex {
+            glm::vec3 position;
+            glm::vec3 normal;
+            glm::vec2 texCoord;
+        };
+        std::vector<Vertex> vertices((division.x + 1) * (division.y + 1));
+        for (int i = 0; i < vertices.size(); i++) {
+            vertices[i] = {
+                positions[i], { 0.0f, 0.0f, 1.0f }, texCoords[i]
+            };
+        }
+
+        /* Indices */
+        std::vector<unsigned int> indices(6 * division.x * division.y);
+
+        for (int i = 0; i < division.y; i++) {
+            int base = (division.x + 1) * i;
+            int baseNext = (division.x + 1) * (i + 1);
+            int indexBase = division.x * i;
+
+            for (int j = 0; j < division.x; j++) {
+                int index = 6 * (indexBase + j);
+
+                indices[index] = base + j;
+                indices[index + 1] = baseNext + j;
+                indices[index + 2] = base + j + 1;
+
+                indices[index + 3] = base + j + 1;
+                indices[index + 4] = baseNext + j;
+                indices[index + 5] = baseNext + j + 1;
+            }
+        }
+
+        m_vertices = core::VertexBuffer::Builder()
+                                .setBuffer(vertices.data(), vertices.size() * sizeof(Vertex), GL_STATIC_DRAW)
+                                .addAttribute<float>(0, 3)
+                                .addAttribute<float>(1, 3)
+                                .addAttribute<float>(2, 2)
+                                .build();
+        
+        m_indices = std::move(indices);
         
         return *this;
     }
@@ -93,6 +162,7 @@ namespace cabin::utils {
     Shape::Builder& Shape::Builder::asShpere(float radius, uint32_t division) {
         constexpr float PI = glm::pi<float>();
 
+        /* Vertices */
         std::vector<glm::vec2> texCoords(2 * division * division);
         std::vector<glm::vec3> positions(2 * division * division);
         for (size_t i = 0; i < division; i++) {
@@ -116,7 +186,21 @@ namespace cabin::utils {
                 texCoords[index] = texCoord;
             }
         }
-        
+
+        struct alignas(4) Vertex {
+            glm::vec3 position;
+            glm::vec3 normal;
+            glm::vec2 texCoord;
+        };
+
+        std::vector<Vertex> vertices(2 * division * division);
+        for (int i = 0; i < vertices.size(); i++) {
+            vertices[i] = {
+                positions[i], positions[i], texCoords[i]
+            };
+        }
+
+        /* Indices */
         std::vector<unsigned int> indices(6 * (2 * division - 1) * (division - 1));
         for (unsigned int i = 0; i < division - 1; i++) {
             for (unsigned int j = 0; j < 2 * division - 1; j++) {
@@ -136,22 +220,6 @@ namespace cabin::utils {
             }
         }
 
-        struct alignas(4) Vertex {
-            glm::vec3 position;
-            glm::vec3 normal;
-            glm::vec2 texCoord;
-        };
-        std::vector<Vertex> vertices(6 * (2 * division - 1) * (division - 1));
-        
-        for (size_t i = 0; i < 6 * (2 * division - 1) * (division - 1); i += 3) {
-            for (size_t j = 0; j < 3; j++) {
-                size_t index = indices[i + j];
-                vertices[i + j].position = positions[index];
-                vertices[i + j].normal = positions[index];
-                vertices[i + j].texCoord = texCoords[index];
-            }
-        }
-
         m_vertices = core::VertexBuffer::Builder()
                                 .setBuffer(vertices.data(), vertices.size() * sizeof(Vertex), GL_STATIC_DRAW)
                                 .addAttribute<float>(0, 3)
@@ -159,7 +227,7 @@ namespace cabin::utils {
                                 .addAttribute<float>(2, 2)
                                 .build();
         
-        m_count = static_cast<GLsizei>(vertices.size());
+        m_indices = std::move(indices);
         
         return *this;
     }
@@ -167,24 +235,25 @@ namespace cabin::utils {
     Shape Shape::Builder::build() {
         Shape result {};
         result.vertices = std::move(m_vertices);
-        result.count = m_count;
+        result.indices = std::move(m_indices);
 
         return result;
     }
 
     Shape::Shape(Shape&& right) noexcept {
-        count = right.count;
         vertices = std::move(right.vertices);
+        indices = std::move(right.indices);
     }
 
     Shape& Shape::operator=(Shape&& right) noexcept {
-        count = right.count;
         vertices = std::move(right.vertices);
+        indices = std::move(right.indices);
         return *this;
     }
 
     void Shape::draw() {
         vertices.bind();
-        glDrawArrays(GL_TRIANGLES, 0, count);
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), 
+                       GL_UNSIGNED_INT, indices.data());
     }
 }
--- src/cabin/utils/shape.h
+++ src/cabin/utils/shape.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <glm/glm.hpp>
 #include "cabin/core/vertexbuffer.h"
 
@@ -13,15 +14,48 @@ namespace cabin::utils {
             Builder(Builder&&) = delete;
             Builder(const Builder&) = delete;
 
+            /** Create the shape as a cube.
+             * @note Default parameters:
+             *  - The size is 2 x 2 x 2.
+             *  - Center at (0, 0, 0).
+             */
             Builder& asCube();
-            Builder& asPlane();
+
+            /** Create the shape as a square.
+             * @note Default parameters:
+             *  - The size is 2 x 2.
+             *  - Center at (0, 0, 0).
+             *  - At z = 0 plane.
+             */
+            Builder& asSquare();
+
+            /** Create the shape as a plane.
+             * @note Default parameters:
+             *  - Center at (0, 0, 0).
+             *
+             *  @param size     The size of plane.
+             *  @param division The number of divided parts.
+             *
+             *  @note To make sure the divided part is a square,
+             *        `division.x * size.y == division.y * size.x`
+             *        should be guaranteed.
+             */
+            Builder& asPlane(glm::vec2 size, glm::vec<2, int> division);
+
+            /** Create the shape as a plane.
+             * @note Default parameters:
+             * - Ceneter at (0, 0, 0).
+             * 
+             * @param radius   The radius of sphere.
+             * @param division The division level, or the number of divided parts.
+             */
             Builder& asShpere(float radius, uint32_t division = 32);
 
             Shape build();
 
         private:
-            GLsizei m_count;
             core::VertexBuffer m_vertices {};
+            std::vector<unsigned int> m_indices {};
         };
 
     public:
@@ -35,7 +69,7 @@ namespace cabin::utils {
         void draw();
 
     public:
-        GLsizei count;
         core::VertexBuffer vertices {};
+        std::vector<unsigned int> indices {};
     };
 }
